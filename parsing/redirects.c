da2cc6cfcc6fb7e38d51688b87cd33128fd09e6d
alchrist <alchrist@student.21-school.ru>
2021-07-12 06:16:24 +0000

    add double left redirecdt

--- parsing/redirects.c
+++ parsing/redirects.c
@@ -18,6 +18,49 @@ void	get_redirects(t_msh *msh, t_cmnd *cmnd, bool is_fork)
 		redirect = redirect->next;
 	}
 	dups_input_output(msh, cmnd, is_fork);
-
 }
 
+void double_left_arrow(t_msh *msh, t_cmnd *cmnd, bool is_fork)
+{
+	int	fd[2];
+	int	pid;
+
+	if (pipe(fd) < 0)
+	{
+		if (is_fork)
+			ft_critical_error(NULL, NULL);
+		else
+			ft_raise_error(msh, NULL, NULL);
+	}
+	pid = fork();
+	if (pid)
+	{
+		wait(0);
+		cmnd->in = fd[0];
+		close(fd[1]);
+	}
+	else
+		double_left_arrow_read(p, fd);
+}
+
+void	double_left_arrow_read(t_msh *msh, t_cmnd *cmnd, bool is_fork)
+{
+	char	*line;
+	int		res;
+
+	res = 1;
+	while (res)
+	{
+		ft_putstr_fd("heredoc> ", 1);
+		res = ne_gnl(0, &line, p->stop_word);
+		if (res < 0)
+			ft_raise_error(NULL, NULL);
+		if (res)
+			ft_putendl_fd(line, fd[1]);
+		free(line);
+	}
+	p->infile = NULL;
+	close(fd[0]);
+	close(fd[1]);
+	exit(0);
+}
